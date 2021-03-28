dd020e34cf829a277c600acdd93b428f1571e7e2
Oscar Gichana <ossygicchy@gmail.com>
2021-03-28 12:53:23 +0300

    adding index and navbar template forms

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    """Post Model."""
+
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='posts/photos')
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return title and username"""
+        return "{} by @{}".format(self.title, self.profile.user.username)
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http  import HttpResponse
+import datetime as dt
+from django.http  import HttpResponse,Http404
+from .models import Post
 
 # Create your views here.
+def index(request):
+    title = 'O_world'
+    return render(request, 'index.html')
