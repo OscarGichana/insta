f5cc2b573cb087ded556e24d52efc2addb315fd8
Oscar Gichana <ossygicchy@gmail.com>
2021-04-04 22:45:22 +0300

    adding urls for navigation

--- posts/urls.py
+++ posts/urls.py
@@ -8,6 +8,16 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns=[
     url(r'^$',views.index,name='ftPic'),
     url(r'^new/comment/(?P<image_id>\d+)', views.new_comment, name='new-comment'),
+    url(r'^image/(?P<image_id>\d+)',views.single,name = 'single'),
+    url(r'^like/(\d+)',views.like,name = 'like'),
+    url(r'^profile',views.profile,name = 'profile'),
+    url(r'^follow/(\d+)',views.follow,name = 'follow'),
+    url(r'^name', views.post_detail, name='post_detail'),
+    url(r'^user_profile/(?P<image_id>\d+)',views.user_profile,name = 'user_profile'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^new/image$', views.new_image, name='new-image')
+
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
