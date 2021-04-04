3409d4e408d4c4f12c21f0733977738d30d4f3d6
Oscar Gichana <ossygicchy@gmail.com>
2021-04-04 22:43:09 +0300

    adding view to search images.

--- posts/views.py
+++ posts/views.py
@@ -61,6 +61,21 @@ def image(request,image_id):
     return render(request,"index.html", {"image":image})
 
 
+@login_required(login_url='/accounts/login/')
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_name(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+
+
 
 @login_required(login_url='/accounts/login/')
 def new_image(request):
